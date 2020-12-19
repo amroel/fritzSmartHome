4460721e24c7eb92de80b1fd066a93599957356b
Amro El-Fakharany <amro00765@gmail.com>
2020-12-19 10:58:51 +0100

    Took Rfc2898DeriveBytes from dotnet core repository and removed the 8 bytes restriction on salt

---
+++ src/FritzSmartHome.FritzBox/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FritzSmartHome.FritzBox
+{
+#nullable enable
+
+	internal static class Utils
+	{
+		[return: NotNullIfNotNull("src")]
+		public static byte[]? CloneByteArray(this byte[]? src)
+		{
+			if (src == null)
+			{
+				return null;
+			}
+
+			return (byte[])(src.Clone());
+		}
+	}
+}
